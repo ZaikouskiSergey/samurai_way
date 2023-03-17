2049fb12f2c535ded86d63398c385d133157d46f
Sergey <Zaikouski.sergey@gmail.com>
2023-03-17 12:52:28 +0200

    '14 lesson. CSS-modules'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://s3-eu-west-1.amazonaws.com/tpd/logos/5be01d787b5e5b0001ebb6bb/0x0.png'/>
         </header>
     )
