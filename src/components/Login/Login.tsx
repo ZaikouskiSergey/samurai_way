952542719483ca01521c6031c41cbe1ac78651f2
Sergey <Zaikouski.sergey@gmail.com>
2023-07-11 15:41:06 +0300

    75 lesson: add component Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
+export const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <input placeholder={'login'}/>
+            </div>
+            <div>
+                <input placeholder={'password'}/>
+            </div>
+            <div>
+                <input type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>login</button>
+            </div>
+        </form>
+    )
+}
 export const Login = () => {
     return (
         <div>
-            Login
+            <h1>Login</h1>
+            <LoginForm/>
         </div>
     );
 };
-
 export default Login;
