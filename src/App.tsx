7f8ddf84518d58925dfde10919ed9159355f53c2
Sergey <Zaikouski.sergey@gmail.com>
2023-07-24 11:48:33 +0300

    80 lesson: changed App

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,30 @@ import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 
-const App = () => {
-    return (
-        <Router>
-            <div className='app-wrapper'>
-                <HeaderContainer/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path="/profile/:userId?"
-                           render={() => <ProfileContainer/>}/>
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div className='app-wrapper'>
+                    <HeaderContainer/>
+                    <Navbar/>
+                    <div className='app-wrapper-content'>
+                        <Route path="/profile/:userId?"
+                               render={() => <ProfileContainer/>}/>
 
-                    <Route path="/dialogs"
-                           render={() => <
-                               DialogsContainer/>}/>
-                    <Route path="/news" render={() => <News/>}/>
-                    <Route path="/music" render={() => <Music/>}/>
-                    <Route path="/settings" render={() => <Settings/>}/>
-                    <Route path="/users" render={() => <UsersContainer/>}/>
-                    <Route path="/login" render={() => <LoginPage/>}/>
+                        <Route path="/dialogs"
+                               render={() => <
+                                   DialogsContainer/>}/>
+                        <Route path="/news" render={() => <News/>}/>
+                        <Route path="/music" render={() => <Music/>}/>
+                        <Route path="/settings" render={() => <Settings/>}/>
+                        <Route path="/users" render={() => <UsersContainer/>}/>
+                        <Route path="/login" render={() => <LoginPage/>}/>
 
+                    </div>
                 </div>
-            </div>
-        </Router>);
+            </Router>);
+    }
 }
+
 export default App;
