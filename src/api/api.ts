eb50fb836727d200cf9eae757b0e3affb4a72164
Sergey <Zaikouski.sergey@gmail.com>
2023-08-02 18:16:22 +0300

    95 lesson: github deploy

--- src/api/api.ts
+++ src/api/api.ts
@@ -19,7 +19,7 @@ const instance = axios.create({
     baseURL: `https://social-network.samuraijs.com/api/1.0/`,
     withCredentials: true,
     headers: {
-        "API-KEY": "81e784df-c3e2-43a6-8fc4-5f9fa65a1919"
+        "API-KEY": "bf538cde-b670-4c39-8701-bf855b3be1fa"
     }
 })
 export const usersAPI = {
